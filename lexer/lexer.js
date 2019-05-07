8a32a18db6c11192d2b8117d642f8cddbf58c023
Roberto Salgado <rsalgado@vsiteam.com>
2019-05-07 13:25:52 -0500

    Implement Lexer#nextToken() method

--- lexer/lexer.js
+++ lexer/lexer.js
@@ -1,3 +1,7 @@
+const token = require('../token/token.js');
+const tokenType = token.tokenTypes;
+
+
 class Lexer {
 
   constructor(input) {
@@ -22,8 +26,56 @@ class Lexer {
   }
 
   nextToken() {
+    let token;
     
+    switch(this.ch) {
+//      case:
+//        break;
+      case '=':
+        token = newToken(tokenType.ASSIGN, this.ch);
+        break;
+
+      case ';':
+        token = newToken(tokenType.SEMICOLON, this.ch);
+        break;
+
+      case '(':
+        token = newToken(tokenType.LPAREN, this.ch);
+        break;
+
+      case ')':
+        token = newToken(tokenType.RPAREN, this.ch);
+        break;
+
+      case ',':
+        token = newToken(tokenType.COMMA, this.ch);
+        break;
+
+      case '+':
+        token = newToken(tokenType.PLUS, this.ch);
+        break;
+
+      case '{':
+        token = newToken(tokenType.LBRACE, this.ch);
+        break;
+
+      case '}':
+        token = newToken(tokenType.RBRACE, this.ch);
+        break;
+
+      case 0:
+        token = {type: tokenType.EOF, literal: ""};
+        break;
+    }
+
+    this.readChar();
+    return token;
   }
+}
+
+
+function newToken(tokenType, char) {
+  return {type: tokenType, literal: char};
 }
 
 
