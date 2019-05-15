0cc4d1a52557804cb95bc33d0ea9063fa6cf6cf1
Roberto Salgado <rsalgado03@gmail.com>
2019-05-15 17:31:47 -0500

    Update REPL to do RPPL and include parsing error messages

--- repl/repl.js
+++ repl/repl.js
@@ -1,25 +1,51 @@
 const readline = require('readline');
 const Lexer = require('../lexer/lexer');
 const Token = require('../token/token');
+const Parser = require('../parser/parser');
 const tokenType = Token.tokenTypes;
 
 
+const MONKEY_FACE = `
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \\/  .-. .-.  \\/ .. \\
+ | |  '|  /   Y   \\  |'  | |
+ | \\   \\  \\ 0 | 0 /  /   / |
+  \\ '- ,\\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\\ '-''
+       |  \\._   _./  |
+       \\   \\ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+const GREETING = `
+Hello, this the Monkey programming language!
+Feel free to type in commands
+`;
+
 const PROMPT = ">> ";
+
+
 let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: PROMPT
 });
 
-
+console.log(GREETING);
 rl.prompt();
+
+
 rl.on("line", (line) => {
   let lexer = new Lexer(line);
-  
-  let token = lexer.nextToken();
-  while(token.type !== tokenType.EOF) {
-    console.log(token);
-    token = lexer.nextToken();
+  let parser = new Parser(lexer);
+  let program = parser.parseProgram();
+
+  if (parser.errors.length !== 0) {
+    printParserErrors(parser.errors);
+  }
+  else {
+    console.log(program.toString());
   }
 
   rl.prompt();
@@ -29,3 +55,13 @@ rl.on("close", () => {
   console.log("Closing...");
   process.exit(0);
 });
+
+
+function printParserErrors(errors) {
+  let output = errors.map(msg => `\t${msg}`).join("\n");
+  console.log(MONKEY_FACE);
+  console.log("Woops! We ran into some monkey business here!");
+  console.log("parser errors:");
+
+  console.log(output, "\n");
+}
